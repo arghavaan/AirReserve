a67933fb61126aa464e3663aa7ff8ce9fd80b900
DESKTOP-J61P4OR\Virupax <virupax.work@gmail.com>
2020-12-07 23:27:50 -0500

    #46 Refactor code from Sprint 2

--- src/main/java/edu/uncc/teamfive/airreserve/Services/APIServices.java
+++ src/main/java/edu/uncc/teamfive/airreserve/Services/APIServices.java
@@ -23,22 +23,36 @@ public class APIServices {
 
 	private static final String QUERYSEPARATOR = "?";
 	private static final String FORWARDSLASHSEPARATOR = "/";
+	private static final String RAPIDAPIHOSTHEADER = "x-rapidapi-host";
+	private static final String RAPIDAPIKEYHEADER = "x-rapidapi-key";
+	private static final String RAPIDAPIHOST = "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com";
+	private static final String RAPIDAPIKEY = "d81debeabemsh298ee99a6fadd38p1c39d1jsnf31e18d621be";
+	private static final String RAPIDAPISERVICELINK = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/";
+	private static final String RAPIDAPIAUTOSUGGESTLINK = RAPIDAPISERVICELINK + "autosuggest/v1.0/USA/USD/en-US/?query=";
+	private static final String RAPIDAPIQUOTESLINK = RAPIDAPISERVICELINK + "browsequotes/v1.0/US/USD/en-US";
+	
+	
+	private String getPlacesService(String placeName) throws IOException {
+		String jsonData ="";
+		OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(RAPIDAPIAUTOSUGGESTLINK + placeName)
+                .get()
+                .addHeader(RAPIDAPIHOSTHEADER, RAPIDAPIHOST)
+                .addHeader(RAPIDAPIKEYHEADER, RAPIDAPIKEY)
+                .build();
+        Response response = client.newCall(request).execute();
+        if(response.isSuccessful()){
+            jsonData = response.body().string();
+        }
+        return jsonData;
+	}
+	
 	
 	public FastPlace[] getPlaces(String name) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/USA/USD/en-US/?query=" + name)
-                .get()
-                .addHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
-                .addHeader("x-rapidapi-key", "d81debeabemsh298ee99a6fadd38p1c39d1jsnf31e18d621be")
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        if(response.isSuccessful()){
-            String jsonData = response.body().string();
-            JSONObject Jobject = new JSONObject(jsonData);
+		String responseJsonData = getPlacesService(name);
+        if(responseJsonData.isEmpty() != Boolean.TRUE){
+            JSONObject Jobject = new JSONObject(responseJsonData);
             JSONArray Jarray = Jobject.getJSONArray("Places");
             List<FastPlace> placelist = new ArrayList<FastPlace>();
             for (int i = 0; i < Jarray.length(); i++) {
@@ -54,20 +68,9 @@ public class APIServices {
         return new FastPlace[0];
 	}
 	
-	public List<QuoteViewModel> getQuotes(Map<String, Object> model) throws IOException {
-        
-		List<QuoteViewModel> quoteslist = new ArrayList<QuoteViewModel>();
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US").newBuilder();
-        
-        String originplace = model.get("from").toString();
-        String destinationplace = model.get("to").toString();
-        String outboundpartialdate = model.get("departure").toString();
-        String inboundpartialdate = "";
-        if(model.get("options").toString() == "RoundTrip") {
-        	inboundpartialdate = model.get("return").toString();
-        }        
- 
-
+	private String getQuotesService(String originplace, String destinationplace, String outboundpartialdate, String inboundpartialdate) throws IOException {
+		String jsonData = "";
+		HttpUrl.Builder urlBuilder = HttpUrl.parse(RAPIDAPIQUOTESLINK).newBuilder();
         String url = urlBuilder.build().toString() + FORWARDSLASHSEPARATOR + originplace + FORWARDSLASHSEPARATOR + destinationplace + FORWARDSLASHSEPARATOR + outboundpartialdate +  QUERYSEPARATOR + "inboundpartialdate=" + inboundpartialdate;
         
         OkHttpClient client = new OkHttpClient();
@@ -75,15 +78,32 @@ public class APIServices {
         Request request = new Request.Builder()
                 .url(url)
                 .get()
-                .addHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
-                .addHeader("x-rapidapi-key", "d81debeabemsh298ee99a6fadd38p1c39d1jsnf31e18d621be")
+                .addHeader(RAPIDAPIHOSTHEADER, RAPIDAPIHOST)
+                .addHeader(RAPIDAPIKEYHEADER, RAPIDAPIKEY)
                 .build();
-
         Response response = client.newCall(request).execute();
-
+		
         if(response.isSuccessful()){
-            String jsonData = response.body().string();
-            JSONObject Jobject = new JSONObject(jsonData);
+            jsonData = response.body().string();        	
+        }
+        return jsonData;
+	}
+	
+	
+	
+	public List<QuoteViewModel> getQuotes(Map<String, Object> model) throws IOException {
+		List<QuoteViewModel> quoteslist = new ArrayList<QuoteViewModel>();
+        String originplace = model.get("from").toString();
+        String destinationplace = model.get("to").toString();
+        String outboundpartialdate = model.get("departure").toString();
+        String inboundpartialdate = "";
+        if(model.get("options").toString() == "RoundTrip") {
+        	inboundpartialdate = model.get("return").toString();
+        }        
+		String responseJsonData = getQuotesService(originplace, destinationplace, outboundpartialdate, inboundpartialdate);
+
+		if(responseJsonData.isEmpty() != Boolean.TRUE){
+            JSONObject Jobject = new JSONObject(responseJsonData);
             
             JSONArray placesJarray = Jobject.getJSONArray("Places");
             List<Place> placelist = new ArrayList<Place>();

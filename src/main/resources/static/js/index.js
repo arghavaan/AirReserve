fa8ba01cee281e162e24b8b808ba961f6049d347
arghavaan <aebrahi1@uncc.edu>
2020-12-12 16:36:10 -0500

    added wizard for payment and info

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -10,6 +10,68 @@ function setDetails(i){
 
 $(document).ready(function(){
 
+
+	var current_fs, next_fs, previous_fs; //fieldsets
+	var opacity;
+
+	$(".next").click(function(){
+
+		current_fs = $(this).parent().parent();
+		next_fs = $(this).parent().parent().next();
+
+//Add Class Active
+		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+//show the next fieldset
+		next_fs.show();
+//hide the current fieldset with style
+		current_fs.animate({opacity: 0}, {
+			step: function(now) {
+// for making fielset appear animation
+				opacity = 1 - now;
+
+				current_fs.css({
+					'display': 'none',
+					'position': 'relative'
+				});
+				next_fs.css({'opacity': opacity});
+			},
+			duration: 600
+		});
+	});
+
+	$(".previous").click(function(){
+
+		current_fs = $(this).parent().parent();
+		previous_fs = $(this).parent().parent().prev();
+
+//Remove class active
+		$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+//show the previous fieldset
+		previous_fs.show();
+
+//hide the current fieldset with style
+		current_fs.animate({opacity: 0}, {
+			step: function(now) {
+// for making fielset appear animation
+				opacity = 1 - now;
+
+				current_fs.css({
+					'display': 'none',
+					'position': 'relative'
+				});
+				previous_fs.css({'opacity': opacity});
+			},
+			duration: 600
+		});
+	});
+
+	$(".submit").click(function(){
+		return false;
+	})
+
+
 	jQuery.ui.autocomplete.prototype._resizeMenu = function () {
 		var ul = this.menu.element;
 		ul.outerWidth(this.element.outerWidth());
